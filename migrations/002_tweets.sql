758a112cfca7ae5c0e1452153b6a9994b24c94dd
Paul Schütte <paul@hanseartic.de>
2022-08-15 12:40:47 +0200

    Add tweets table

---
+++ migrations/002_tweets.sql
@@ -0,0 +1,17 @@
+--------------------------------------------------------------------------------
+-- Up
+--------------------------------------------------------------------------------
+PRAGMA FOREIGN_KEYS = ON;
+CREATE TABLE IF NOT EXISTS tweets(
+                         id TEXT PRIMARY KEY NOT NULL,
+                         timestamp TEXT NOT NULL ON CONFLICT REPLACE DEFAULT CURRENT_TIMESTAMP,
+                         latest_payment TEXT NOT NULL,
+                         FOREIGN KEY(latest_payment) REFERENCES payments (id)
+) WITHOUT ROWID;
+CREATE INDEX idx_tweet_ts ON tweets(timestamp);
+
+--------------------------------------------------------------------------------
+-- Down
+--------------------------------------------------------------------------------
+DROP INDEX IF EXISTS idx_tweet_ts;
+DROP TABLE IF EXISTS tweets;
